d5f10a7b81f494c5d8938fd0960c11b8c70eeab6
Alex O <53114650+alexonati@users.noreply.github.com>
2022-03-28 18:34:10 +0300

    Update homework_1.py

--- homework_15_03_2022/homework_1.py
+++ homework_15_03_2022/homework_1.py
@@ -2,6 +2,7 @@ your_initial_list = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
 asc_sorted_list_1 = your_initial_list.copy()
 asc_sorted_list_1.sort()
 print(your_initial_list)
+print(your_initial_list.sort(reverse=TRUE))
 print(asc_sorted_list_1)
 print(asc_sorted_list_1[1::2])
 print(asc_sorted_list_1[2::3])
